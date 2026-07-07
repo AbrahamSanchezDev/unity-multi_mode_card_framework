be33a0d0318722b9d0c254bac8d77ece3e36fc85
Abraham Sanchez <abraham_gto@hotmail.com>
2026-07-07 12:14:09 -0600

    BlackjackView Tests added

---
+++ Assets/Project/Scripts/Tests/EditMode/Presentation/ViewClassForTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using UnityEngine.UIElements;
+
+namespace CardFramework.Tests.EditMode.Presentation {
+    public class ViewClassForTests {
+        protected void SimulateButtonClick(Button button) {
+            if (button == null || button.clickable == null) return;
+
+            // Obtenemos el método interno 'Invoke' de la clase Clickable a través de reflexión
+            var invokeMethod = typeof(Clickable).GetMethod("Invoke",
+                System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public);
+
+            if (invokeMethod != null) {
+                using (var clickEvent = ClickEvent.GetPooled()) {
+                    clickEvent.target = button;
+                    // Invocamos el método directamente sobre el objeto 'clickable' del botón
+                    invokeMethod.Invoke(button.clickable, new object[] { clickEvent });
+                }
+            }
+            else {
+                Assert.Fail("No se pudo resolver el método interno 'Invoke' en Clickable para simular el click.");
+            }
+        }
+    }
+}
